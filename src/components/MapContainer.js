ec2adc4d6777144050d862036b69c74579efc921
Mandy Kong <mandy@mapzen.com>
2017-07-05 15:55:07 -0400

    Created MapContainer
    MapContainer returns Map component and passes in config as props to Map component

---
+++ src/components/MapContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Map from './Map'
+import { connect } from 'react-redux'
+
+class MapContainer extends React.Component {
+	render() {
+		return (
+			<Map className="map-container" config={this.props.config}/> 
+		)
+	}
+}
+
+function mapStateToProps(state) {
+  return {
+    config: state.config,
+  }
+}
+
+export default connect(mapStateToProps)(MapContainer)
