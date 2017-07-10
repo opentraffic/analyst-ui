d917c4cf1d4e69951131b6fd95a5056857c324ec
Mandy Kong <mandy@mapzen.com>
2017-07-10 09:32:46 -0400

    Created reducer for dates chosen
    Stores unix timestamp of startDate and endDate chosen from calendar

---
+++ src/store/reducers/date.js
@@ -0,0 +1,19 @@
+const initialState = {
+	startDate: null,
+	endDate: null,
+}
+
+const date = (state = initialState, action) => {
+	switch (action.type) {
+		case 'SET_DATE':
+			return {
+				...state,
+				startDate: action.startDate,
+				endDate: action.endDate
+			}
+		default:
+			return state
+	}
+}
+
+export default date 
