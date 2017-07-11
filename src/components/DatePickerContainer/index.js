0147530ec8dd098866bf5c242ec0f0a7df96ff60
Mandy Kong <mandy@mapzen.com>
2017-07-11 12:37:17 -0400

    Fixed spaces/indents

--- src/components/DatePickerContainer/index.js
+++ src/components/DatePickerContainer/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { DateRangePicker, isInclusivelyBeforeDay } from 'react-dates' 
+import { DateRangePicker, isInclusivelyBeforeDay } from 'react-dates'
 import moment from 'moment'
 import store from '../../store'
 import 'react-dates/lib/css/_datepicker.css'
@@ -17,13 +17,14 @@ class DatePickerContainer extends React.Component {
 		this.state = {
 			focusedInput: null
 		}
-    this.handleDateChange = this.handleDateChange.bind(this)
+		this.handleDateChange = this.handleDateChange.bind(this)
 	}
 
 	handleDateChange(date) {
 		// if start/end is not equal to null then take unix of timestamp of start/end
-		const start = date.startDate instanceof(moment) ? date.startDate.valueOf() : null 
+		const start = date.startDate instanceof(moment) ? date.startDate.valueOf() : null
 		const end = date.endDate instanceof(moment) ? date.endDate.valueOf() : null
+
 		this.props.dispatch({
 			type: 'SET_DATE',
 			startDate: start,
@@ -32,35 +33,37 @@ class DatePickerContainer extends React.Component {
 	}
 
 	render() {
-		// Changing unix timestamp back into moment to initialize props of DateRangePicker
+		// changing unix timestamp back into moment to initialize props of DateRangePicker
 		function changeUnixToMoment(timestamp) {
 			return (timestamp === null) ? null : moment(timestamp)
 		}
 
-		const start = store.getState().date.startDate 
+		const start = store.getState().date.startDate
 		const end = store.getState().date.endDate
-		const today = moment(); 
+		const today = moment();
 
-    // If info panel needed pass into renderCalendarInfo() as function 
+    // If info panel needed, return in renderCalendarInfo
 		const infoPanel = (
-			<div className="info-panel">	
-				This is where the info panel is.
+			<div className="info-panel">
+				This is an info panel
 			</div>
 		)
 
 		return (
-			<DateRangePicker
-       startDate={changeUnixToMoment(start)}  // momentPropTypes.momentObj or null,
-       endDate={changeUnixToMoment(end)} // momentPropTypes.momentObj or null,
-			 numberOfMonths={1}
-			 isOutsideRange={day => !isInclusivelyBeforeDay(day, today)}
-			 onDatesChange={this.handleDateChange}
-			 focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
-			 onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
-			 renderCalendarInfo={() => { return infoPanel }}
-       showClearDates={true}
-       reopenPickerOnClearDates={true}
-		  />
+			<div className="date-picker">
+				<DateRangePicker
+					startDate={changeUnixToMoment(start)}  // momentPropTypes.momentObj or null,
+					endDate={changeUnixToMoment(end)} // momentPropTypes.momentObj or null,
+					numberOfMonths={1}
+					isOutsideRange={day => !isInclusivelyBeforeDay(day, today)}
+					onDatesChange={this.handleDateChange}
+					showClearDates={true}
+					reopenPickerOnClearDates={true}
+					focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+					onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
+					renderCalendarInfo={() => { return infoPanel }}
+				/>
+			</div>
 		)
 	}
 }
