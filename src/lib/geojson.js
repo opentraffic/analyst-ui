8e2caac9675883d78c95843fe826dcdc4f35c476
Lou Huang <lou@louhuang.com>
2017-07-31 17:38:23 -0400

    Rewrite merge() with functional array methods; add test

--- src/lib/geojson.js
+++ src/lib/geojson.js
@@ -6,24 +6,22 @@ import normalize from '@mapbox/geojson-normalize'
  * features in all files.  The objects can be any valid GeoJSON root object,
  * including FeatureCollection, Feature, and Geometry types.
  *
- * This has been copied from the module `@mapbox/geojson-merge@1.0.2`. The
+ * This has been adapted from the module `@mapbox/geojson-merge@1.0.2`. The
  * original module is not included because it includes a dependency on `JSONStream`
  * which is not actually needed here, and contains a shebang line that breaks
  * Webpack. (https://github.com/dominictarr/JSONStream/issues/126)
  *
- * @param {Array<Object>} inputs a list of GeoJSON objects of any type
- * @return {Object} a geojson FeatureCollection.
+ * @param {Array<Object>} - inputs a list of GeoJSON objects of any type
+ * @return {Object} - a geojson FeatureCollection.
  */
 export function merge (inputs) {
-  var output = {
+  const normalized = inputs.map(normalize)
+  const output = {
     type: 'FeatureCollection',
-    features: []
+    features: normalized.reduce((features, geo) => {
+      return features.concat(geo.features)
+    }, [])
   }
-  for (var i = 0; i < inputs.length; i++) {
-    var normalized = normalize(inputs[i])
-    for (var j = 0; j < normalized.features.length; j++) {
-      output.features.push(normalized.features[j])
-    }
-  }
+
   return output
 }
