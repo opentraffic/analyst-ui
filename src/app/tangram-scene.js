3d851eb60216557fc9b40dddeafa8a8db36f64e0
Lou Huang <lou@louhuang.com>
2017-08-10 19:23:13 -0400

    Switch back to gray refill for visibility

--- src/app/tangram-scene.js
+++ src/app/tangram-scene.js
@@ -3,8 +3,8 @@ import config from '../config'
 const scene = {
   import: [
     'https://mapzen.com/carto/refill-style/7/refill-style.zip',
-    // 'https://mapzen.com/carto/refill-style/7/themes/gray.zip'
-    'https://mapzen.com/carto/refill-style/7/themes/gray-gold.zip'
+    'https://mapzen.com/carto/refill-style/7/themes/gray.zip'
+    // 'https://mapzen.com/carto/refill-style/7/themes/gray-gold.zip'
   ],
   global: {
     'sdk_mapzen_api_key': config.mapzen.apiKey
