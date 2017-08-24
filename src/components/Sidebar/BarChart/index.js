0c6c2d9a526749b9905c2235731a0ef885238946
Lou Huang <lou@louhuang.com>
2017-08-23 18:16:38 -0700

    Add hourly bar chart

--- src/components/Sidebar/BarChart/index.js
+++ src/components/Sidebar/BarChart/index.js
@@ -3,22 +3,27 @@ import { Segment, Header } from 'semantic-ui-react'
 import dc from 'dc'
 import * as d3 from 'd3'
 import crossfilter from 'crossfilter'
+import 'dc/dc.css'
 import './BarChart.css'
 
 import data from './testdata.json'
 
 export default class BarChart extends React.Component {
   componentDidMount () {
-    this.makeTemporaryDcChart()
+    const chartData = crossfilter(data.hours)
+
+    this.makeDailyChart(chartData)
+    this.makeHourlyChart(chartData)
+
+    dc.renderAll()
   }
 
-  makeTemporaryDcChart = () => {
+  makeDailyChart = (chartData) => {
     const dayLabel = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ]
     const dailyChart = dc.barChart(this.dailyChart)
-    const ndx = crossfilter(data.hours)
 
     // Magnitude dimension is `dayOfWeek` property, subtract 1 to force 0-index
-    const dayCount = ndx.dimension(d => (d.dayOfWeek - 1))
+    const dayCount = chartData.dimension(d => (d.dayOfWeek - 1))
     const dayCountGroup = dayCount.group().reduce(
       // Callback for when data is added to the current filter results
       function (p, v) {
@@ -68,23 +73,80 @@ export default class BarChart extends React.Component {
       .renderHorizontalGridLines(true)
       .elasticY(true)
       // Filter brush
-      .brushOn(true)
+      .brushOn(false)
       .round(n => Math.floor(n) + 0.5)
       .alwaysUseRounding(true)
 
     // Customize axes
     dailyChart.xAxis().tickFormat(d => dayLabel[d])
     dailyChart.yAxis().ticks(5)
+  }
 
-    dc.renderAll()
+  makeHourlyChart = (chartData) => {
+    const hourlyChart = dc.barChart(this.hourlyChart)
+    const hourCount = chartData.dimension(d => d.hourOfDay)
+    const hourCountGroup = hourCount.group().reduce(
+      // Callback for when data is added to the current filter results
+      function (p, v) {
+        p.count += v.c
+        p.sum += v.s
+        if (p.count > 0) {
+          p.avg = (p.sum / p.count)
+        } else {
+          p.avg = 0
+        }
+        return p
+      },
+      // Callback for when data is removed from the current filter results
+      function (p, v) {
+        p.count -= v.c
+        p.sum -= v.s
+        if (p.count > 0) {
+          p.avg = (p.sum / p.count)
+        } else {
+          p.avg = 0
+        }
+        return p
+      },
+      // Initialize `p`
+      function () {
+        return {
+          count: 0,
+          sum: 0,
+          avg: 0
+        }
+      }
+    )
+
+    hourlyChart
+      .width(430)
+      .height(150)
+      .margins({top: 5, right: 10, bottom: 20, left: 40})
+      .dimension(hourCount)
+      .group(hourCountGroup)
+      .transitionDuration(0)
+      .centerBar(true)
+      .valueAccessor(p => p.value.avg)
+      .gap(5)
+      .brushOn(false)
+      .x(d3.scale.linear().domain([0.5, 24.5]))
+      .renderHorizontalGridLines(true)
+      .elasticY(true)
+      .xAxis().tickFormat()
   }
 
   render () {
     return (
       <Segment>
-        <Header>Speed by day of week</Header>
+        <div className="barchart-daily">
+          <Header>Speed by day of week</Header>
+          <div ref={(ref) => { this.dailyChart = ref }} />
+        </div>
 
-        <div ref={(ref) => { this.dailyChart = ref }} />
+        <div className="barchart-hourly">
+          <Header>Speed by hour of day</Header>
+          <div ref={(ref) => { this.hourlyChart = ref }} />
+        </div>
       </Segment>
     )
   }
