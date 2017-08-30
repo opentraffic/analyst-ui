db76c96bb98bdd2adf63b248a29eab93c0ce72e8
Lou Huang <lou@louhuang.com>
2017-08-30 17:21:46 -0400

    Remove temporary "budget" time picker

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
 import ErrorMessage from './ErrorMessage'
 import AnalysisName from './AnalysisName'
 import ModeSelect from './ModeSelect'
@@ -9,7 +9,6 @@ import BarChart from './BarChart'
 import Legend from './Legend'
 import ExportData from './ExportData'
 import './Sidebar.css'
-import store from '../../store'
 
 class Sidebar extends React.Component {
   constructor (props) {
@@ -20,28 +19,12 @@ class Sidebar extends React.Component {
     }
   }
 
-  onChangeSelect = (event) => {
-    const value = event.target.value
-    this.setState({ hourValue: value })
-    store.dispatch({
-      type: 'set_hour',
-      value: Number(value)
-    })
-  }
-
   render () {
     let errors = null
     if (this.props.errors.length > 0) {
       errors = this.props.errors.map(error => (
         <ErrorMessage header="Routing error" message={error.error} />
       ))
-    }
-
-    function generateHours () {
-      const arr = [...Array(168).keys()]
-      return arr.map(i => {
-        return <option value={i} key={i}>{i}</option>
-      })
     }
 
     return (
@@ -51,10 +34,6 @@ class Sidebar extends React.Component {
         <ModeSelect />
         <Segment>
           <DatePickerContainer className="date-picker" />
-          <Header as="h3">Budget time picker</Header>
-          <select value={this.state.value} onChange={this.onChangeSelect}>
-            {generateHours()}
-          </select>
         </Segment>
         <BarChart />
         <Legend />
