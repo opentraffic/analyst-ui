54c01dd9e5c7ab18ad3bdb9937f1467357d01113
Mandy <mandy@mapzen.com>
2017-09-08 15:15:50 -0400

    testing

--- src/components/Map/Route/RouteMultiLine/index.js
+++ src/components/Map/Route/RouteMultiLine/index.js
@@ -3,6 +3,29 @@ import PropTypes from 'prop-types'
 import { Polyline, LayerGroup } from 'react-leaflet'
 import { getSpeedColor } from '../../../../lib/color-ramps'
 
+function getSegmentWidth(zoom) {
+  const widths = [[17, 4], [18, 18], [20, 48]]
+  if (zoom < 13) { return 5 }
+  for (let i = 0; i < widths.length; i++) {
+    const zoomValue = widths[i][0]
+    const width = widths[i][1]
+    if (zoom === zoomValue) {
+      return width
+    } else if (zoom < widths[0][0]) {
+      return widths[0][1]
+    } else if (zoom > widths[widths.length-1][0]) {
+      return widths[widths.length-1][1]
+    } else if (zoom < zoomValue) {
+      const y = widths[i - 1][1]
+      const x = widths[i - 1][0]
+      const slope = (width - y) / (zoomValue - x)
+      const b = y - (x * slope)
+      const newValue = slope * zoom + b
+      return newValue
+    }
+  }
+}
+
 export default class RouteMultiLine extends React.PureComponent {
   static propTypes = {
     segments: PropTypes.arrayOf(PropTypes.shape({
@@ -23,7 +46,7 @@ export default class RouteMultiLine extends React.PureComponent {
         <Polyline
           positions={segment.coordinates}
           color="#222"
-          weight={(segment.speed === 0) ? 5 : 7}
+          weight={getSegmentWidth(this.props.zoom) + 3}
           key={index}
         />
       )
@@ -36,7 +59,7 @@ export default class RouteMultiLine extends React.PureComponent {
         <Polyline
           positions={segment.coordinates}
           color={getSpeedColor(segment.speed)}
-          weight={(segment.speed === 0) ? 2 : 4}
+          weight={getSegmentWidth(this.props.zoom)}
           onMouseDown={this.props.insertWaypoint}
           key={index}
         />
@@ -46,7 +69,6 @@ export default class RouteMultiLine extends React.PureComponent {
 
   render () {
     if (!this.props.segments || this.props.segments.length === 0) return null
-
     return (
       <LayerGroup>
         {this.createPolylineBorder()}
