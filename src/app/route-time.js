96c8d97109b38474dbce2f9a9c8287765e816fb1
Duane Gearhart <duane@mapzen.com>
2017-09-20 15:41:49 -0400

    fixed test errors

--- src/app/route-time.js
+++ src/app/route-time.js
@@ -1,49 +1,51 @@
 export function getRouteTime (traceAttributes) {
-  var edges = traceAttributes.edges;
-  var routeTime = 0;
-  var edgeTime = 0;
-  var prevEdge = null;
-  var currEdge = null;
+  var edges = traceAttributes.edges
+  var routeTime = 0
+  var edgeTime = 0
+  var prevEdge = null
+  var currEdge = null
   for (var i = 0; i < edges.length; i++) {
-    currEdge = edges[i];
+    currEdge = edges[i]
     // TODO handle skipping edges
 
     // if traffic segments exist
-    if (edge.traffic_segments)
-      edgeTime = getTrafficSegmentsTime(edge);
-    else
-      edgeTime = getEdgeElapsedTime(prevEdge, currEdge);
+    if (currEdge.traffic_segments) {
+      edgeTime = getTrafficSegmentsTime(currEdge)
+    } else {
+      edgeTime = getEdgeElapsedTime(prevEdge, currEdge)
+    }
 
-    routeTime += edgeTime;
-    prevEdge = currEdge;
+    routeTime += edgeTime
+    prevEdge = currEdge
   }
-  return routeTime;
+  return routeTime
 }
 
-
-function getEdgeElapsedTime(prevEdge, currEdge) {
-  if (prevEdge)
-    return prevEdge.end_node.elapsed_time - currEdge.end_node.elapsed_time;
-  else
-    return currEdge.end_node.elapsed_time;
+function getEdgeElapsedTime (prevEdge, currEdge) {
+  if (prevEdge) {
+    return prevEdge.end_node.elapsed_time - currEdge.end_node.elapsed_time
+  } else {
+    return currEdge.end_node.elapsed_time
+  }
 }
 
-function getTrafficSegmentsTime(edge) {
-  var edgeTime = 0;
+function getTrafficSegmentsTime (edge) {
+  var edgeTime = 0
+  var segment
   for (var i = 0; i < edge.traffic_segments.length; i++) {
-    segment = traffic_segments[i];
+    segment = edge.traffic_segments[i]
     edgeTime += (edge.length * (segment.end_percent - segment.begin_percent) / getSpeedFromDataTilesForSegmentId(segment.segment_id) * 3600) + getIntersectionTime()
   }
-  return edgeTime;
+  return edgeTime
 }
 
 // TODO this function will be replaced by Lou's function
-function getSpeedFromDataTilesForSegmentId(segment_id) {
-  return 30;
+function getSpeedFromDataTilesForSegmentId (segmentId) {
+  return 30
 }
 
-function getIntersectionTime() {
+function getIntersectionTime () {
   // TODO will need another function from Lou
-  //return previous segment, current segment delay time;
-  return 0;
+  // return previous segment, current segment delay time;
+  return 0
 }
