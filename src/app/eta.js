e73c6845a3ea60b3241c4fc33218065a96ef2f3b
kdiluca <kristen@mapzen.com>
2017-09-20 09:28:33 -0400

    increment current edge to find next edge with subsequent traffic segments

--- src/app/eta.js
+++ src/app/eta.js
@@ -6,12 +6,12 @@ routeAttributesResp.edges.ForEach(function eta(edge, index)) {
   //if previous edge has traffic segments AND current edge is internal, turn lane, or roundabout
   if (edge.traffic_segments && (routeAttributesResp.edges[index+1].internal_intersection || routeAttributesResp.edges[index+1].use(turn_channel) ||  routeAttributesResp.edges[index+1].roundabout)){
     //increment current edge to find next edge with subsequent traffic segments;
-    next_edge = routeAttributesResp.edges[index+2].traffic_segments;
-    routeAttributesResp.edges[index+1] == next_edge;
+    while (next_edge == null && index <= routeAttributesResp.edges.size())
+      next_edge = routeAttributesResp.edges[index++].traffic_segments;
     
     //if not able to find next edge with subsequent traffic segments
-    if (next_edge == null) {
-      //Reset current edge to previous edge + 1
+    //Reset current edge to previous edge + 1
+    if (next_edge == null)
       routeAttributesResp.edges[index+1] == edge.traffic_segments + 1;
     }
   }
