6c1a63e7870559044c7445e282d68dd46c745680
kdiluca <kristen@mapzen.com>
2017-09-25 13:33:44 -0400

    WIP hooking up analyst ui get speed tile functions

--- src/app/__tests__/processing.test.js
+++ src/app/__tests__/processing.test.js
@@ -106,14 +106,14 @@ describe('getSubtileForSegmentId', () => {
   })
 })
 
-// describe('getSpeedFromDataTilesForSegmentId', () => {
-//   it('returns null if no value is found', () => {
-//     expect(getSpeedFromDataTilesForSegmentId(49152)).toEqual(null)
-//   })
-// })
-//
-// describe('getNextSegmentDelayFromDataTiles', () => {
-//   it('returns null if no value is found', () => {
-//     expect(getNextSegmentDelayFromDataTiles(49152)).toEqual(null)
-//   })
-// })
+describe('getSpeedFromDataTilesForSegmentId', () => {
+  it('returns null if no value is found', () => {
+    expect(getSpeedFromDataTilesForSegmentId(49152)).toEqual(null)
+  })
+})
+
+describe('getNextSegmentDelayFromDataTiles', () => {
+  it('returns null if no value is found', () => {
+    expect(getNextSegmentDelayFromDataTiles(49152)).toEqual(null)
+  })
+})
