75bc780f4e7fca3145dfc8eaf7eae4b03b6ac588
Drew Dara-Abrams <drew@mapzen.com>
2017-09-29 15:41:55 -0700

    route queries: display times as minutes and seconds

--- src/components/Sidebar/ETAView/index.js
+++ src/components/Sidebar/ETAView/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Segment, Header } from 'semantic-ui-react'
+import humanizeDuration from 'humanize-duration'
 
 class ETAView extends React.Component {
   static propTypes = {
@@ -11,11 +12,13 @@ class ETAView extends React.Component {
 
   render () {
     if (this.props.baselineTime && this.props.trafficRouteTime) {
+      const baselineTimeToDisplay = humanizeDuration(this.props.baselineTime * 1000, { round: true })
+      const trafficRouteTimeToDisplay = humanizeDuration(this.props.trafficRouteTime * 1000, { round: true })
       return (
         <Segment>
           <Header as="h3">Route time</Header>
-          <p><strong>Baseline time</strong> {this.props.baselineTime}s</p>
-          <p><strong>Traffic route time</strong> {this.props.trafficRouteTime.toFixed(2)}s</p>
+          <p><strong>Given speed limits</strong>: {baselineTimeToDisplay}</p>
+          <p><strong>Given measured traffic</strong>: {trafficRouteTimeToDisplay}</p>
         </Segment>
       )
     }
