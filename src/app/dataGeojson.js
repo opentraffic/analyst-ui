3727fcd80d7177d4ecb0e11d7af5abef42e9d973
Mandy <mandy@mapzen.com>
2017-10-04 17:40:40 -0400

    getting data availability geojson

---
+++ src/app/dataGeojson.js
@@ -0,0 +1,31 @@
+import L from 'leaflet'
+import config from '../config'
+import store from '../store'
+import { recenterMap } from '../store/actions/map'
+import { setDateRange } from '../store/actions/date'
+
+const DATA_GEOJSON_URL = config.dataGeojson
+
+export function setDataGeojson() {
+  window.fetch(DATA_GEOJSON_URL)
+    .then(response => response.json())
+    .then(results => {
+      const coverage = L.geoJSON(results, {
+        style: function (feature) {
+          return {color: feature.properties.color}
+        },
+        onEachFeature: function (feature, layer) {
+          layer.on({ click: featureClicked })
+        }
+      })
+      window.dataGeojson = coverage
+      coverage.addTo(window.map)
+    })
+}
+
+function featureClicked(event) {
+  const latlng = [event.latlng.lat, event.latlng.lng]
+  store.dispatch(recenterMap(latlng, 10))
+  const { rangeStartDate, rangeEndDate } = event.target.feature.properties
+  store.dispatch(setDateRange(rangeStartDate, rangeEndDate))
+}
