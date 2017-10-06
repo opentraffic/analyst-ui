3f86edce67fb716f642a36cfc69084fc67eb4083
Peter Richardson <peter@mapzen.com>
2017-10-05 22:18:25 -0700

    fix syntax bug

--- src/app/tangram-scene.js
+++ src/app/tangram-scene.js
@@ -6,7 +6,8 @@ import { getColorAtIndexInVec3 } from '../lib/color-ramps'
 const scene = {
   import: [
     'https://mapzen.com/carto/refill-style/7/refill-style.zip',
-    'https://mapzen.com/carto/refill-style/7/themes/gray.zip',
+    'https://mapzen.com/carto/refill-style/7/themes/gray.zip'
+  ],
   global: {
     'sdk_mapzen_api_key': config.mapzen.apiKey
   },
