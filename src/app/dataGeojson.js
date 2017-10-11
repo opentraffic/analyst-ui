41faf227a3d5922feaa8033efcffb74f2a8e2dac
Mandy <mandy@mapzen.com>
2017-10-11 11:16:47 -0400

    dataCoverage added to map on load if no bounds or route; when region selected, check which feature box it is in and store dateRange in redux

--- src/app/dataGeojson.js
+++ src/app/dataGeojson.js
@@ -1,9 +1,10 @@
 import L from 'leaflet'
 import store from '../store'
 import config from '../config'
+import { parseQueryString } from '../lib/url-state'
 import { recenterMap } from '../store/actions/map'
-import { setDateRange } from '../store/actions/date'
-import { setDataGeoJSON } from '../store/actions/view'
+import { setDateRange, clearDateRange } from '../store/actions/date'
+import { setDataGeoJSON } from '../store/actions/dataGeoJSON'
 
 const DATA_GEOJSON_URL = config.dataGeojson
 
@@ -21,7 +22,10 @@ export function setDataCoverage () {
         }
       })
       window.dataCoverage = coverage
-      coverage.addTo(window.map)
+      if (parseQueryString('waypoints') === null &&
+          parseQueryString('rw') === null) {
+        coverage.addTo(window.map)
+      }
     })
 }
 
@@ -29,5 +33,34 @@ function featureClicked (event) {
   const latlng = [event.latlng.lat, event.latlng.lng]
   store.dispatch(recenterMap(latlng, 10))
   const { rangeStartDate, rangeEndDate } = event.target.feature.properties
-  store.dispatch(setDateRange(rangeStartDate, rangeEndDate))
 }
+
+// geojson feature.geometry.coordinates are in lng, lat
+export function getDateRange(query) {
+  const northEast = query.getNorthEast()
+  const southWest = query.getSouthWest()
+
+  const features = store.getState().dataAvailability.dataGeoJSON.features
+  let found = false
+
+  for (let i = 0; i < features.length; i++) {
+    const coordinates = features[i].geometry.coordinates[0]
+    const northEastTile = {
+      lat: coordinates[2][1],
+      lng: coordinates[2][0]
+    }
+    const southWestTile = {
+      lat: coordinates[0][1],
+      lng: coordinates[0][0]
+    }
+    if (northEast.lat < northEastTile.lat && northEast.lng < northEastTile.lng &&
+        southWest.lat > southWestTile.lat && southWest.lng > southWestTile.lng) {
+      const { rangeStartDate, rangeEndDate } = features[i].properties
+      store.dispatch(setDateRange(rangeStartDate, rangeEndDate))
+      found = true
+      break
+    }
+  }
+  if (!found) { store.dispatch(clearDateRange()) }
+}
+
