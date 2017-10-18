bcc57c5c46b7be1ec4f25d1fdcb4b89a04a79ede
Duane Gearhart <duane@mapzen.com>
2017-10-18 12:09:54 -0400

    updated to osmlr v1.1

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ const config = {
     center: [0, 120],
     zoom: 3
   },
-  osmlrTileUrl: 'https://osmlr-tiles.s3.amazonaws.com/v1.0/geojson/',
+  osmlrTileUrl: 'https://osmlr-tiles.s3.amazonaws.com/v1.1/geojson/',
   dataGeojson: 'https://s3.amazonaws.com/referencetiles-prod/coverage_map.geojson',
   historicSpeedTileUrl: 'https://speedtiles-prod.s3-accelerate.amazonaws.com/',
   nextSegmentTileUrl: 'https://speedtiles-prod.s3-accelerate.amazonaws.com/',
