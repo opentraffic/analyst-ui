cd22e7a81a46f5288e3aff2db993c3f9bc2955bb
kdiluca <kristen@mapzen.com>
2017-10-25 11:29:33 -0400

    added conditional labels to bar charts; moved things around; fixed formatting

--- src/components/Sidebar/TimeFilters/index.js
+++ src/components/Sidebar/TimeFilters/index.js
@@ -97,23 +97,31 @@ export class TimeFilters extends React.Component {
   render () {
     return (
       <Segment>
-        <Header>Segment speeds</Header>
+        <div className="radio">
+          <strong>
+            <Radio
+              toggle
+              label="Compare against reference speeds"
+              checked={this.props.refSpeedComparisonEnabled}
+              onChange={(event, data) => this.props.dispatch(setRefSpeedComparisonEnabled(data.checked))}
+            />
+          </strong>
+        </div>
+        <Divider/>
         <div className="timefilter-daily">
-          <strong>Percent change in Speed by day-of-week</strong>
+          <Header as="h3">
+            { (this.props.refSpeedComparisonEnabled) ? 'Percent change in speed by day-of-week' : 'Average by day-of-week' }
+          </Header>
           <div ref={(ref) => { this.dailyChartEl = ref }} />
         </div>
 
         <div className="timefilter-hourly">
-          <strong>Percent change in Speed by hour-of-day</strong>
+          <Header as="h3">
+            { (this.props.refSpeedComparisonEnabled) ? 'Percent change in speed by hour-of-day' : 'Average by hour-of-day' }
+          </Header>
           <div ref={(ref) => { this.hourlyChartEl = ref }} />
         </div>
         <Divider />
-        <Radio
-          toggle
-          label="Compare against reference speeds"
-          checked={this.props.refSpeedComparisonEnabled}
-          onChange={(event, data) => this.props.dispatch(setRefSpeedComparisonEnabled(data.checked))}
-        />
       </Segment>
     )
   }
