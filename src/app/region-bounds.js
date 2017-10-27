eadbf34128cd929d4b4f40e83b164a0905dd3eaf
Mandy <mandy@mapzen.com>
2017-10-27 14:12:03 -0400

    fixed lint-js errors; removed previous shades functions

--- src/app/region-bounds.js
+++ src/app/region-bounds.js
@@ -210,7 +210,6 @@ export function drawBounds ({ west, south, east, north }) {
     [south, east]
   ]).addTo(map)
   rect.enableEdit()
-  // createShades(rect)
   shades = new L.LeafletShades({bounds: rect})
 
   if (!handlersAdded) {
@@ -221,87 +220,3 @@ export function drawBounds ({ west, south, east, north }) {
   storeBounds(rect.getBounds())
   compareRegionAndMap(rect)
 }
-
-// function createShades (rect) {
-//   // If there are shades already, don't create more
-//   if (shades) { return }
-//   // Set shades to true since now shades exist
-//   shades = true
-//   const regionSelector = map._panes.overlayPane
-//   map._shadeContainer = L.DomUtil.create('div', 'leaflet-areaselect-container', regionSelector)
-//   map._topShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-//   map._bottomShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-//   map._leftShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-//   map._rightShade = L.DomUtil.create('div', 'leaflet-areaselect-shade', map._shadeContainer)
-//   updateShades(rect)
-// }
-
-// // Setting the dimensions (width, height) and position (top, left) of a shade
-// function setDimensions (element, dimension) {
-//   element.style.width = dimension.width + 'px'
-//   element.style.height = dimension.height + 'px'
-//   element.style.top = dimension.top + 'px'
-//   element.style.left = dimension.left + 'px'
-// }
-
-// // When map is zoomed in/out and/or moved, get the offset for the origin zoom and lat/lng values
-// function getOffset () {
-//   // Getting the transformation value through style attributes
-//   let transformation = map.getPanes().mapPane.style.transform
-//   const startIndex = transformation.indexOf('(')
-//   const endIndex = transformation.indexOf(')')
-//   transformation = transformation.substring(startIndex + 1, endIndex).split(',')
-//   const offset = {
-//     x: Number(transformation[0].slice(0, -2) * -1),
-//     y: Number(transformation[1].slice(0, -2) * -1)
-//   }
-//   return offset
-// }
-
-// // Calculating values for the dimensions and positions of each shade
-// function updateShades (rect) {
-//   // Checking if there are shades to update
-//   if (!shades) return
-
-//   const rectBounds = rect.getBounds()
-//   const size = map.getSize()
-//   const offset = getOffset()
-
-//   const northEastPoint = map.latLngToContainerPoint(rectBounds.getNorthEast())
-//   const southWestPoint = map.latLngToContainerPoint(rectBounds.getSouthWest())
-
-//   setDimensions(map._topShade, {
-//     width: size.x,
-//     height: (northEastPoint.y < 0) ? 0 : northEastPoint.y,
-//     top: offset.y,
-//     left: offset.x
-//   })
-
-//   setDimensions(map._bottomShade, {
-//     width: size.x,
-//     height: size.y - southWestPoint.y,
-//     top: southWestPoint.y + offset.y,
-//     left: offset.x
-//   })
-
-//   setDimensions(map._leftShade, {
-//     width: (southWestPoint.x < 0) ? 0 : southWestPoint.x,
-//     height: southWestPoint.y - northEastPoint.y,
-//     top: northEastPoint.y + offset.y,
-//     left: offset.x
-//   })
-
-//   setDimensions(map._rightShade, {
-//     width: size.x - northEastPoint.x,
-//     height: southWestPoint.y - northEastPoint.y,
-//     top: northEastPoint.y + offset.y,
-//     left: northEastPoint.x + offset.x
-//   })
-// }
-
-// export function removeShades () {
-//   // If shades exist remove it
-//   if (shades) { L.DomUtil.remove(map._shadeContainer) }
-//   // Set shades to false, since shades are now removed
-//   shades = false
-// }
