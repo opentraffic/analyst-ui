40f977e1316552a096e02c350ae136eba2d77c36
Drew Dara-Abrams <drew@mapzen.com>
2017-11-01 09:36:21 -0700

    CSV export code improvements

--- src/lib/exporter.js
+++ src/lib/exporter.js
@@ -1,13 +1,40 @@
 import FileSaver from 'file-saver'
 import _ from 'lodash'
 
+function prepareCsvForExport (data, analysisMode, route) {
+  let rows = []
+  if (analysisMode === 'ROUTE') {
+    rows.push({
+      'row_type': 'route_summary',
+      'baseline_route_time': route.baselineTime,
+      'traffic_route_time': route.trafficRouteTime
+    })
+  }
+  rows = rows.concat(_.map(data.features, (segment) => {
+    var row = {
+      'row_type': 'segment',
+      'osmlr_id': segment.properties.osmlr_id
+    }
+    const daysOfWeek = [null, 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    segment.properties.speedByHour.forEach((speedByHour) => {
+      let dayOfWeek = daysOfWeek[speedByHour.dayOfWeek]
+      row[`average_speed_on_${dayOfWeek}_at_hour_${speedByHour.hourOfDay}`] = speedByHour.speedThisHour
+    })
+    return row
+  }))
+  let str = convertArrayOfObjectsToCsv(rows)
+  return str
+}
+
 /**
+ * Takes in an array of objects, creates CSV in a big string that includes
+ * header columns for all of the possible object keys.
  * Based on https://appendto.com/2017/04/use-javascript-to-export-your-data-as-csv/
  * @param {Object} data
  * @param {String} columnDelimiter
  * @param {String} lineDelimiter
  */
-function convertArrayOfObjectsToCsv (data, columnDelimiter = ',', lineDelimiter = '\n') {
+export function convertArrayOfObjectsToCsv (data, columnDelimiter = ',', lineDelimiter = '\n') {
   var result,
     ctr,
     keys
@@ -56,31 +83,13 @@ export function exportData (obj, name = 'untitled', format = 'geojson', analysis
 
   let blob
 
+  // TODO: include `date` in the export?
+
   if (format === 'geojson') {
     let str = JSON.stringify(obj, null, 2)
     blob = new Blob([str], {type: 'application/json;charset=utf-8'})
   } else if (format === 'csv') {
-    let rows = []
-    if (analysisMode === 'ROUTE') {
-      rows.push({
-        'row_type': 'route_summary',
-        'baseline_route_time': route.baselineTime,
-        'traffic_route_time': route.trafficRouteTime
-      })
-    }
-    rows = rows.concat(_.map(obj.features, (segment) => {
-      var row = {
-        'row_type': 'segment',
-        'osmlr_id': segment.properties.osmlr_id
-      }
-      const daysOfWeek = [null, 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-      segment.properties.speedByHour.forEach((speedByHour) => {
-        let dayOfWeek = daysOfWeek[speedByHour.dayOfWeek]
-        row[`average_speed_on_${dayOfWeek}_at_hour_${speedByHour.hourOfDay}`] = speedByHour.speedThisHour
-      })
-      return row
-    }))
-    let str = convertArrayOfObjectsToCsv(rows)
+    let str = prepareCsvForExport(obj, analysisMode, route)
     blob = new Blob([str], {type: 'text/csv;charset=utf-8'})
   } else {
     throw new Error('exportData() only supports GeoJSON or CSV formats')
