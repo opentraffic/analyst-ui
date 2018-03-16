6c9f1a1682b1fc770faaf1d0690af0f76f0d9f66
Steven Ottens <github@minst.net>
2018-03-16 15:18:50 +0100

    updated coverage map

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ const config = {
     zoom: 3
   },
   osmlrTileUrl: 'https://osmlr-tiles.s3.amazonaws.com/v1.1/geojson/',
-  dataGeojson: 'https://s3.amazonaws.com/referencetiles-prod/coverage_map.geojson',
+  dataGeojson: 'https://s3.amazonaws.com/referencetiles-prod/coverage.geojson',
   historicSpeedTileUrl: 'https://speedtiles-prod.s3-accelerate.amazonaws.com/',
   nextSegmentTileUrl: 'https://speedtiles-prod.s3-accelerate.amazonaws.com/',
   refSpeedTileUrl: 'https://referencetiles-prod.s3-accelerate.amazonaws.com/',
